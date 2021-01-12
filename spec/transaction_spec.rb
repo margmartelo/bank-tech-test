b3042b646515c6238435c0645f23899767668292
Margarida Martelo <margmartelo@gmail.com>
2021-01-12 15:32:27 +0000

    Adding second and third unit test for Transactions: instantiationwith and without providing transaction date

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,4 +5,16 @@ describe Transaction do
     transaction = Transaction.new("debit",100, 100)
     expect(transaction.balance).to eq 100
   end
+
+  it 'can be instantiated with a provided date' do
+    transaction = Transaction.new("debit",100, 100, 11/03/2020)
+    expect(transaction.date).to eq 11/03/2020
+  end
+
+  it 'will assume current date by default if date not provided' do
+    transaction = Transaction.new("debit",100, 100)
+    @time_now = Time.now.strftime('%d/%m/%Y')
+    expect(transaction.date).to eq @time_now
+  end
+
 end
