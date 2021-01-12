9c33cd7cb77bae03b55fc297df04001403f0bf8d
Margarida Martelo <margmartelo@gmail.com>
2021-01-12 21:56:53 +0000

    Refactoring unit test to Formatter in order to make it more comprehensive

--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -2,14 +2,15 @@ require 'formatter'
 require 'transaction'
 
 describe Formatter do
-  formatter = Formatter.new
-  transactions = [
-    Transaction.new("credit", 100, 1000, "12/01/2021")
-  ]
-  describe '#view' do
-    it 'shows the account statement' do
+  describe 'print statement functionality' do
+    it 'prints transactions in reverse chronological order' do
+      formatter = Formatter.new
+      transactions = [
+        Transaction.new("credit", 100, 1000, "11/01/2021"),
+        Transaction.new("debit", 50, 950, "12/01/2021")
+      ]
       expect { formatter.print_statement(transactions) }.to output(
-        "Date || Credit || Debit || Balance\n12/01/2021 || 100.00 ||  || 1000.00\n").to_stdout
-    end
+      "Date || Credit || Debit || Balance\n12/01/2021 ||  || 50.00 || 950.00\n11/01/2021 || 100.00 ||  || 1000.00\n").to_stdout
+   end
   end
 end
